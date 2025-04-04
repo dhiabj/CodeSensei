51c84e74637ba34472c0ac89a9aa3bbd87622ea9
Dhia Bejaoui <dhia.bejaoui.db@gmail.com>
2025-04-04 14:53:26 +0100

    fix backend deploy issues

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-// Middleware
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
-  })
-);
-app.options('*', cors());
+app.use(cors());
+
 app.use(express.json());
 
-// Routes
 const aiRoutes = require('./routes/ai.routes');
 app.use('/api/ai', aiRoutes);
+
 app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 
 module.exports = app;

dc3e6edf306bdd9a1db0b516dfd48238da6e1fc3
dhiabj <dhia.bejaoui.db@gmail.com>
2025-04-14 11:51:38 +0100

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,19 @@ require('./config/passport');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
+// Configure CORS
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
+app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 
